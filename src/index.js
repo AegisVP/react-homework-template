76523429e13b89fba0a768c2af4d6c792a3e7595
Vladislav Pysarenko <vlad@pysarenko.com>
2023-01-14 01:12:58 +0100

    personal configure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
+import { theme } from './components/Common/Theme.jsx';
 import './index.css';
+
+///////////////////
+// Маршрутизация //
+///////////////////
+//
+// Если приложение использует библиотеку react-router-dom для маршрутизации,
+// необходимо дополнительно настроить компонент < BrowserRouter >,
+// передав в пропе basename точное название твоего репозитория.
+//
+// <BrowserRouter basename="/_____repo_name______">
+//   <App />
+// </BrowserRouter>
+//
+///////////////////
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
